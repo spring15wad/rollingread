087731199236939afa9ba4a80f6535c6e5ee3c69
Mike Freeman <mfreema5@hawk.iit.edu>
2015-03-29 20:41:21 -0500

    Ran ‘rake db:migrate’. Populated seeds.rb.

---
+++ db/schema.rb
@@ -0,0 +1,61 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150330013600) do
+
+  create_table "assignments", force: :cascade do |t|
+    t.string   "short_assignment"
+    t.text     "assignment_details"
+    t.string   "url"
+    t.date     "due_date"
+    t.date     "completed"
+    t.integer  "source_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  add_index "assignments", ["source_id"], name: "index_assignments_on_source_id"
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "course_number"
+    t.string   "short_course"
+    t.text     "full_course"
+    t.integer  "meet_days"
+    t.integer  "semester_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "courses", ["semester_id"], name: "index_courses_on_semester_id"
+
+  create_table "semesters", force: :cascade do |t|
+    t.string   "short_semester"
+    t.text     "full_semester"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "sources", force: :cascade do |t|
+    t.string   "short_source"
+    t.text     "source_details"
+    t.boolean  "dummy"
+    t.integer  "course_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "sources", ["course_id"], name: "index_sources_on_course_id"
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Semester.create!([
+  {short_semester: "S15", full_semester: "Spring 15", start_date: "2015-01-12", end_date: "2015-05-02"}
+])
+Course.create!([
+  {course_number: "COM531", short_course: "Web Apps", full_course: "Web Application Development", meet_days: 2, semester_id: 1},
+  {course_number: "COM580", short_course: "Game Design", full_course: "Fundamentals of Game Design", meet_days: 5, semester_id: 1}
+])
+Source.create!([
+  {short_source: "Challenges…", source_details: "\"Challenges for game designers\". Brathwaite, Brenda, and Ian Schreiber.", dummy: false, course_id: 2},
+  {short_source: "Rules of Play", source_details: "\"Rules of Play\". Katie Salen Tekinbas and Eric Zimmerman.", dummy: false, course_id: 2},
+  {short_source: "Learn to…", source_details: "\"Learn to Program (2nd ed.)\". Pine, C. ", dummy: false, course_id: 1},
+  {short_source: "Agile Web Dev…", source_details: "\"Agile Web Development with Rails 4.\" Ruby, S., Thomas, D., and D. H. Hansson. ", dummy: false, course_id: 1},
+  {short_source: "URL", source_details: "URL", dummy: true, course_id: 1},
+  {short_source: "URL", source_details: "URL", dummy: true, course_id: 2}
+])
+Assignment.create!([
+  {short_assignment: "JavaScript-p1", assignment_details: "Marijn Haverbeke, Eloquent JavaScript, Part 1", url: "http://eloquentjavascript.net/", due_date: "2015-03-24", completed: false, source_id: 5},
+  {short_assignment: "Ch.22-24", assignment_details: "Chapters 22, 23, 24", url: "", due_date: "2015-03-04", completed: false, source_id: 2},
+  {short_assignment: "Ch.10-11", assignment_details: "Chapter 10 and 11", url: "", due_date: "2015-03-02", completed: true, source_id: 1},
+  {short_assignment: "Ch.11-12", assignment_details: "Chapter 11 and 12", url: "", due_date: "2015-03-31", completed: false, source_id: 4},
+  {short_assignment: "Ch.24-25", assignment_details: "Chapter 24 and 25", url: "", due_date: "2015-03-31", completed: false, source_id: 4}
+])
