f3ab9ee69821098cc3e1275730f15c109aaccc3a
Mike Freeman <mfreema5@hawk.iit.edu>
2015-03-30 20:31:35 -0500

    Add grouping to ‘task_controller.rb’.

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,4 +1,6 @@
 class TaskController < ApplicationController
   def index
+    @assignments = Assignment.order('due_date')
+    @grouped_assignments = @assignments.group_by { |assignment| assignment.due_date }
   end
 end
