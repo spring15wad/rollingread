8ae1d10e39d85b8194384ded4d65ad30fd350e7f
Mike Freeman <mfreema5@hawk.iit.edu>
2015-05-05 10:32:12 -0500

    Turns out, you must have ‘provider’ in your User table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+  has_many :semesters
+  has_many :courses, through: :semesters
+
+  def self.find_or_create_from_auth_hash(hash)
+    if @user = find_by_provider_and_uid(hash['provider'], hash['uid'])
+      @user
+    else
+      @user = create(username: hash['info']['nickname'], provider: hash['provider'], uid: hash['uid'])
+    end
+  end
+
+
+
+
+
 end
--- db/migrate/20150505144320_create_users.rb
+++ db/migrate/20150505144320_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
+      t.string :provider
       t.string :uid
 
       t.timestamps null: false
